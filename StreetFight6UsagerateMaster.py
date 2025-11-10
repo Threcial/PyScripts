341d28df41ff0e88ec06ab449cffc4bd146577b2
Threcial <null_d0ud@outlook.com>
2025-11-10 13:17:11 +0800

    add a local database

--- StreetFight6UsagerateMaster.py
+++ StreetFight6UsagerateMaster.py
@@ -9,7 +9,7 @@ import re
 import json
 import matplotlib.pyplot as plt
 from time import sleep
-
+import pymysql
 
 def check_edition():
     url = "https://www.streetfighter.com/6/buckler/zh-hans/stats/usagerate_master"
@@ -86,15 +86,48 @@ class usage:
             print(self.date[i], end="  ")
             print(high_master_usagerate[i][0]['character_alpha'])
 
-
+def con_mysql():
+    conn = pymysql.connect(host='localhost',
+                           user='root',
+                           password='root',
+                           database='street_fighter_6',
+                           charset='utf8mb4')
+    return conn
     
-edition = check_edition()
-json_list = []
-for i in edition:
-    json_list.append([check_json_file(i), i])
-    sleep(0.1)
-a = usage(json_list)
-a.master_show()
-a.high_master_show()
+def insert_sql(conn, json_list):
+    query = 'insert into master_usagerate (character_tool_name, character_alpha, play_rate, previous_rate, play_cnt, total_cnt, date, user_rank) values (%s, %s, %s, %s, %s, %s, %s, %s)' 
+    master_tuple_list = []
+    for i in json_list:
+        date = int(i[1])
+        master_data = i[0]['usagerateData'][0]['val'][0]['val']
+        high_master_data = i[0]['usagerateData'][0]['val'][1]['val']
+        grand_master_data = i[0]['usagerateData'][0]['val'][2]['val']
+        ultimate_master_data = i[0]['usagerateData'][0]['val'][3]['val']
+        for w in master_data:
+            master_tuple_list.append((w['character_tool_name'], w['character_alpha'], w['play_rate'], w['previous_rate'], w['play_cnt'], w['total_cnt'], date, 'master'))
+        for w in high_master_data:
+            master_tuple_list.append((w['character_tool_name'], w['character_alpha'], w['play_rate'], w['previous_rate'], w['play_cnt'], w['total_cnt'], date, 'high_master'))
+        for w in grand_master_data:
+            master_tuple_list.append((w['character_tool_name'], w['character_alpha'], w['play_rate'], w['previous_rate'], w['play_cnt'], w['total_cnt'], date, 'grand_master'))
+        for w in ultimate_master_data:
+            master_tuple_list.append((w['character_tool_name'], w['character_alpha'], w['play_rate'], w['previous_rate'], w['play_cnt'], w['total_cnt'], date, 'ultimate_master'))
+    cursor = conn.cursor()
+    cursor.executemany(query, master_tuple_list)
+    conn.commit()
+    cursor.close()
+    return 1
+
+def main():    
+    edition = check_edition()
+    json_list = []
+    for i in edition:
+        json_list.append([check_json_file(i), i])
+        sleep(0.1)
+    conn = con_mysql()
+    print('ok') if insert_sql(conn, json_list) else print('no')
+    conn.close()
+
+if __name__ == '__main__':
+    main()
 
 #https://www.streetfighter.com/6/buckler/assets/images/material/character/usagerate/card_gouki.jpg
